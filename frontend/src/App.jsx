a61c864b1430ba205cf94cd8ad2f80e52e65f9ea
John Stewartsson J R <stewartsson@gmail.com>
2026-08-28 22:03:54 +0530

    Update API URL to production endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,7 +40,7 @@ import EventsPageLive from "./EventsPage";
 import AnalystPageLive from "./AnalystPage";
 import "./App.css";
 
-const API = "http://127.0.0.1:8000/api";
+const API = "https://enviguard-backend.onrender.com/api";
 
 const HAZARDS = [
   {
